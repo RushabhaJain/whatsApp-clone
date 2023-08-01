95a1416429e159e7b8aca0da8464d2aa1e47168b
RushabhaJain <rushabhajain44@gmail.com>
2023-08-01 19:27:39 +0530

    Updated socket server

--- src/app.js
+++ src/app.js
@@ -1,8 +1,59 @@
-const express = require('express');
-require('dotenv').config(); // load environment variables from .env file
+const { WebSocketServer } = require('ws');
 
-const app = express();
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+const wss = new WebSocketServer({ port: 3000 });
+
+const map = new Map();
+
+wss.on('connection', (socket) => {
+    socket.on('error', console.error);
+
+    socket.on('message', (data) => {
+        try {
+            // Convert the message into JSON
+            const socketData = JSON.parse(data);
+
+            // Validate the data to have action
+            if (socketData.action === null) {
+                socket.send('Please add valid action');
+                return;
+            }
+
+            // Set username
+            if (socketData.action === 'SetUsername') {
+                if (socketData.username === null) {
+                    socket.send('Please add valid name');
+                    return;
+                }
+                map.set(socketData.username, socket);
+            }
+
+            // Handle one-to-one communication
+            if (socketData.action === 'Send') {
+                if (socketData.message === null) {
+                    socket.send('Please add valid message');
+                    return;
+                }
+                if (socketData.to != null && map.has(socketData.to)) {
+                    map.get(socketData.to).send(socketData.message);
+                } else {
+                    // Broadcast the message to all the users
+                    for(let [key, value] of map) {
+                        if (socketData.username !== key) {
+                            value.send(socketData.message);
+                        }
+                    }
+                }
+            }
+            socket.send('success');
+        } catch (e) {
+            console.error(e);
+            socket.send('Please enter valid JSON');
+        }
+
+        console.log('received: %s', data);
+    });
+
+    socket.send('Successfully connected to whatsapp-clone server!');
 });
+
+console.log('server started on port 3000');
